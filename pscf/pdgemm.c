4c089bfa276787503bffc824a849fb208843e425
Hua Huang <huangh223@gatech.edu>
2018-10-07 21:08:14 -0400

    Algorithm 5

--- pscf/pdgemm.c
+++ pscf/pdgemm.c
@@ -7,6 +7,7 @@
 #include <math.h>
 #include <mpi.h>
 #include <sys/time.h>
+#include <omp.h>
 
 #include "pdgemm.h"
 
@@ -14,7 +15,7 @@ static void copyMat(int m, int n, double *From, int ldfrom, double *To, int ldto
 {
     #pragma omp parallel for
     for (int r = 0; r < m; r++)
-        for (int c = 0; c < n; c++) To[r * ldto + c] = From[r * ldfrom + c];
+        memcpy(To + r * ldto, From + r * ldfrom, sizeof(double) * n);
 }
 
 static double get_wtime_sec()
@@ -52,9 +53,49 @@ static void ReduceTo2D(
     }
 }
 
-void ReduceToGrd0(
-    int myrow, int mycol, int mygrd, 
-    int S_len, double *S, MPI_Comm comm_3D
+#define N_DUP    4
+#define N_DUP_05 2
+MPI_Comm    comm_rows[N_DUP], comm_cols[N_DUP], comm_grds[N_DUP], comm_3Ds[N_DUP];
+MPI_Status  status[N_DUP];
+MPI_Request reqs[N_DUP], reqs0[N_DUP];
+int spos[N_DUP + 1], blklen[N_DUP];
+int row_spos[N_DUP + 1], row_blklen[N_DUP];
+int comm_dupped = 0;
+
+static void dup_comms(MPI_Comm comm_row, MPI_Comm comm_col, MPI_Comm comm_grd, MPI_Comm comm_3D, int nrow, int ncol)
+{
+    if (comm_dupped == 1) return;
+    comm_dupped = 1;
+    
+    int remainder  = nrow % N_DUP;
+    int block_size = nrow / N_DUP;
+    for (int i = 0; i < remainder; i++)
+    {
+        row_blklen[i] = block_size + 1;
+        blklen[i] = row_blklen[i] * ncol;
+    }
+    for (int i = remainder; i < N_DUP; i++)
+    {
+        row_blklen[i] = block_size;
+        blklen[i] = row_blklen[i] * ncol;
+    }
+    
+    
+    row_spos[0] = spos[0] = 0;
+    for (int i = 0; i < N_DUP; i++)
+    {
+        MPI_Comm_dup(comm_row, &comm_rows[i]);
+        MPI_Comm_dup(comm_col, &comm_cols[i]);
+        MPI_Comm_dup(comm_grd, &comm_grds[i]);
+        MPI_Comm_dup(comm_3D , &comm_3Ds [i]);
+        spos[i + 1] = spos[i] + blklen[i];
+        row_spos[i + 1] = row_spos[i] + row_blklen[i];
+    }
+}
+
+static void ReduceToGrd0(
+    int myrow, int mycol, int mygrd, int nrows, int ncols, 
+    double *S, double *S_buf, MPI_Comm comm_3D
 )
 {
     // Process (myrow, myrow, mygrd) send to process (myrow, mygrd, 0)
@@ -63,17 +104,55 @@ void ReduceToGrd0(
     {
         if (myrow == mycol) 
         {
+            MPI_Request req;
             int coords[3] = {myrow, mygrd, 0}, dst;
             MPI_Cart_rank(comm_3D, coords, &dst);
-            MPI_Send(S, S_len, MPI_DOUBLE, dst, 0, comm_3D);
+            for (int i = 0; i < N_DUP; i++)
+                MPI_Isend(S + spos[i], blklen[i], MPI_DOUBLE, dst, 0, comm_3Ds[i], &req);
         }
         
         if (mygrd == 0)
         {
-            MPI_Status status;
             int coords[3] = {myrow, myrow, mycol}, src;
             MPI_Cart_rank(comm_3D, coords, &src);
-            MPI_Recv(S, S_len, MPI_DOUBLE, src, 0, comm_3D, &status);
+            if (myrow == mycol) 
+            {
+                for (int i = 0; i < N_DUP; i++)
+                    MPI_Irecv(S_buf + spos[i], blklen[i], MPI_DOUBLE, src, 0, comm_3Ds[i], &reqs0[i]);
+            } else {
+                for (int i = 0; i < N_DUP; i++)
+                    MPI_Irecv(S     + spos[i], blklen[i], MPI_DOUBLE, src, 0, comm_3Ds[i], &reqs0[i]);
+            }
+            MPI_Waitall(N_DUP, &reqs0[0], &status[0]);
+        }
+    }
+}
+
+static void ReduceToGrd0_2(
+    int myrow, int mycol, int mygrd, 
+    int nrows, int ncols, double *S
+)
+{
+    if (mygrd)
+    {
+        if (mycol == mygrd)
+        {
+            for (int i = 0; i < N_DUP; i++)
+            {
+                MPI_Wait(&reqs[i], &status[i]);
+                MPI_Isend(S + spos[i], blklen[i], MPI_DOUBLE, 0, 0, comm_grds[i], &reqs[i]);
+            }
+        }
+    }
+    else
+    {
+        if (mycol)
+        {
+            for (int i = 0; i < N_DUP; i++)
+                MPI_Irecv(S + spos[i], blklen[i], MPI_DOUBLE, mycol, 0, comm_grds[i], &reqs0[i]);
+            MPI_Waitall(N_DUP, &reqs0[0], &status[0]);
+        } else {
+            MPI_Waitall(N_DUP, &reqs[0], &status[0]);
         }
     }
 }
@@ -90,9 +169,10 @@ int pdgemm3D(int myrow, int mycol, int mygrd,
              tmpbuf_t *tmpbuf, double *dgemm_time)
 {
     int ncols0 = nc[0], nrows0 = nr[0];
-    if (dgemm_time != NULL) *dgemm_time = 0.0;
     assert(nrows  == nr[myrow] && ncols == nc[mycol]);
     assert(ncols0 >= ncols && nrows0 >= nrows);
+    
+    dup_comms(comm_row, comm_col, comm_grd, comm_3D, nrows0, ncols0);
 
     double *A   = tmpbuf->A;
     double *S   = tmpbuf->S;
@@ -100,53 +180,99 @@ int pdgemm3D(int myrow, int mycol, int mygrd,
     double *A_i = tmpbuf->A_i;
     double *S_i = tmpbuf->S_i;
     double *C_i = tmpbuf->C_i;
-    double st, et;
+    double st, et, _dgemm_time = 0.0;
 
-    memset(A, 0, sizeof(double) * nrows0 * ncols0);
-    copyMat(nrows, ncols, D_, ncols, A, ncols0);
+	#pragma omp parallel for 
+    for (int r = 0; r < nrows0; r++)
+    {
+        if (r < nrows)
+        {
+            memcpy(A + r * ncols0, D_ + r * ncols, sizeof(double) * ncols);
+            memset(A + r * ncols0 + ncols, 0, sizeof(double) * (ncols0 - ncols));
+        } else {
+            memset(A + r * ncols0, 0, sizeof(double) * ncols0);
+        }
+    }
 
     // 1.1. Replicate D matrix on all planes
-    MPI_Bcast(A, nrows0 * ncols0, MPI_DOUBLE, 0, comm_grd);
+    //MPI_Bcast(A, nrows0 * ncols0, MPI_DOUBLE, 0, comm_grd);
+    for (int i = 0; i < N_DUP; i++)
+        MPI_Ibcast(A + spos[i], blklen[i], MPI_DOUBLE, 0, comm_grds[i], &reqs[i]);
+    //MPI_Waitall(N_DUP, &reqs[0], &status[0]);
 
     // 1.2. Broadcast A_i row
-    if (myrow == mygrd) copyMat(nrows0, ncols0, A, ncols0, &A_i[0], ncols0);
-    MPI_Bcast(&A_i[0], nrows0 * ncols0, MPI_DOUBLE, mygrd, comm_col);
-
-    // 2.1. Do local dgemm
+    if (myrow == mygrd) A_i = A;  // Need not to copy, we won't modify the data later
+    if (myrow == mygrd && myrow > 0)
+    {
+        for (int i = 0; i < N_DUP; i++)
+        {
+            // When the i-th block of A is broadcast, Ibcast it as A_i immediately
+            MPI_Wait(&reqs[i], &status[i]);  
+            MPI_Ibcast(A_i + spos[i], blklen[i], MPI_DOUBLE, mygrd, comm_cols[i], &reqs0[i]);
+        }
+    } else {
+        // Start the Ibcast of B without waiting
+        for (int i = 0; i < N_DUP; i++)
+            MPI_Ibcast(A_i + spos[i], blklen[i], MPI_DOUBLE, mygrd, comm_cols[i], &reqs0[i]);
+        // Wait the broadcast of A to complete
+        MPI_Waitall(N_DUP, &reqs[0], &status[0]);
+    }
+    MPI_Waitall(N_DUP, &reqs0[0], &status[0]);
+    
+    // 2.1 Do local dgemm
     st = get_wtime_sec();
     cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, ncols0, ncols0,
-                ncols0, 1.0, A, ncols0, &A_i[0], ncols0, 0.0, &S_i[0], ncols0);
+                ncols0, 1.0, A, ncols0, A_i, ncols0, 0.0, S_i, ncols0);
     et = get_wtime_sec();
-    if (dgemm_time != NULL) *dgemm_time += et - st;
-    
-    // 2.2. Reduce S_i into a column i on row i
-    MPI_Reduce(&S_i[0], S, nrows0 * ncols0, MPI_DOUBLE, MPI_SUM, myrow, comm_row);
-    
-    // 2.3. Copy S to S_i, ready to broadcast
-    copyMat(nrows0, ncols0, &S[0], ncols0, &S_i[0], ncols0);
-    
-    // 3.1. Broadcast S_i
-    MPI_Bcast(&S_i[0], nrows0 * ncols0, MPI_DOUBLE, mycol, comm_col);
+    _dgemm_time += et - st;
 
-    // 3.2. C_i=A*S_i
+    // 2.2. Reduce S_i into a column i on row i
+    for (int i = 0; i < N_DUP; i++)
+        MPI_Ireduce(S_i + spos[i], S + spos[i], blklen[i], MPI_DOUBLE, MPI_SUM, myrow, comm_rows[i], &reqs[i]);
+    //MPI_Waitall(N_DUP, &reqs[0], &status[0]);
+    
+    // 3.1. Copy S to S_i, ready to broadcast
+    //S_i = S;  // Need not to copy, won't affect ReduceToGrd0
+    
+    // 3.2. Broadcast S_i
+    for (int i = 0; i < N_DUP; i++)
+    {
+        MPI_Wait(&reqs[i], &status[i]);
+        MPI_Ibcast(S + spos[i], blklen[i], MPI_DOUBLE, mycol, comm_cols[i], &reqs[i]);
+    }
+    MPI_Waitall(N_DUP, &reqs[0], &status[0]);
+    
+    // 3.3 C_i=A*S_i
     st = get_wtime_sec();
     cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, ncols0, ncols0,
-                ncols0, 1.0, A, ncols0, &S_i[0], ncols0, 0.0, &C_i[0], ncols0);
+                ncols0, 1.0, A, ncols0, S, ncols0, 0.0, C_i, ncols0);
     et = get_wtime_sec();
-    if (dgemm_time != NULL) *dgemm_time += et - st;
-    
-    // 3.3. Reduce C_i into a column on plane i
-    MPI_Reduce(&C_i[0], C, nrows0 * ncols0, MPI_DOUBLE, MPI_SUM, mygrd, comm_row);
-    
-    // 3.4. Reduce S to plane 0
-    ReduceToGrd0(myrow, mycol, mygrd, nrows0 * ncols0, S, comm_3D);
+    _dgemm_time += et - st;
 
-    // 3.5. Reduce C to plane 0
-    ReduceTo2D(myrow, mycol, mygrd, nrows0, ncols0, C, 0, comm_3D);
+    // 3.4. Reduce C_i into a column on plane i
+    for (int i = 0; i < N_DUP; i++)
+        MPI_Ireduce(C_i + spos[i], C + spos[i], blklen[i], MPI_DOUBLE, MPI_SUM, mygrd, comm_rows[i], &reqs[i]);
+    
+    // 4.1. Reduce S to plane 0
+    ReduceToGrd0(myrow, mycol, mygrd, nrows0, ncols0, S, S_i, comm_3D);
+    if (mygrd == 0 && myrow == mycol && myrow > 0) S = S_i;
 
-    // 4. Copy results to D2 and D3
-    copyMat(nrows, ncols, S, ncols0, D2_, ncols);
-    copyMat(nrows, ncols, C, ncols0, D3_, ncols);
+    // 4.2. Reduce C to plane 0
+    ReduceToGrd0_2(myrow, mycol, mygrd, nrows0, ncols0, C);
+
+    // 4.3. Copy results to D2 and D3
+    if (mygrd == 0)
+    {
+        size_t row_size = ncols * sizeof(double);
+        #pragma omp parallel for
+        for (int irow = 0; irow < nrows; irow++)
+        {
+            memcpy(D2_ + ncols * irow, S + ncols0 * irow, row_size);
+            memcpy(D3_ + ncols * irow, C + ncols0 * irow, row_size);
+        }
+    }
+    
+    if (dgemm_time != NULL) *dgemm_time = _dgemm_time;
 
     return 0;
 }
@@ -216,20 +342,18 @@ void pdgemm3D_2(int myrow, int mycol, int mygrd,
 }
 
 
-void allocate_tmpbuf (int nrows, int ncols, int *nr, int *nc,
-                      tmpbuf_t * tmpbuf)
+void allocate_tmpbuf(int nrows, int ncols, int *nr, int *nc, tmpbuf_t *tmpbuf)
 {
     int ncols0 = nc[0], nrows0 = nr[0];
     assert (ncols0 >= ncols && nrows0 >= nrows);
 
-    int block_size = nrows0 * ncols0;
-    tmpbuf->A   = (double *) _mm_malloc(sizeof(double) * block_size * 6, 64);
-    tmpbuf->S   = tmpbuf->A   + block_size;
-    tmpbuf->C   = tmpbuf->S   + block_size;
-    tmpbuf->A_i = tmpbuf->C   + block_size;
-    tmpbuf->S_i = tmpbuf->A_i + block_size;
-    tmpbuf->C_i = tmpbuf->S_i + block_size;
-	
+    int block_size_align64b = (nrows0 * ncols0 + 7) / 8 * 8;
+    tmpbuf->A   = (double *) _mm_malloc(sizeof(double) * block_size_align64b * 6, 64);
+    tmpbuf->S   = tmpbuf->A   + block_size_align64b;
+    tmpbuf->C   = tmpbuf->S   + block_size_align64b;
+    tmpbuf->A_i = tmpbuf->C   + block_size_align64b;
+    tmpbuf->S_i = tmpbuf->A_i + block_size_align64b;
+    tmpbuf->C_i = tmpbuf->S_i + block_size_align64b;
 
     #pragma omp parallel for schedule(static)
     #pragma simd
@@ -245,7 +369,7 @@ void allocate_tmpbuf (int nrows, int ncols, int *nr, int *nc,
 }
 
 
-void dealloc_tmpbuf (tmpbuf_t * tmpbuf)
+void dealloc_tmpbuf(tmpbuf_t *tmpbuf)
 {
     _mm_free(tmpbuf->A);
 }
